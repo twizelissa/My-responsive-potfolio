5f32d11ca6ece1807e5f3cbe5ff53995ffa889a0
twizeyimana.elissa <twizelissa@gmail.com>
2023-03-11 15:28:55 +0200

    added swiper style and removed bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER PROJECTS ===============*/
- let swiperProjects = new Swiper(".project__container", {
+ let swiperProjects = new Swiper(".projects__container", {
     loop:true,
     spaceBetween:24,
     
